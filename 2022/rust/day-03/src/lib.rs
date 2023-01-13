01a7be52eccae8c2a24c96e15ef3b4c99493baa0
Philipp Kurz <kurz_philipp@gmx.de>
2023-01-13 16:39:53 +0100

    Set up day 3

---
+++ 2022/rust/day-03/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn process_part_one(input: &str) -> Result<String, &'static str> {
+    Ok(input.to_string())
+}
+
+pub fn process_part_two(input: &str) -> Result<String, &'static str> {
+    Ok(input.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw";
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(process_part_one(INPUT).unwrap(), "157");
+    }
+
+    // #[test]
+    // fn test_part_two() {
+    //     assert_eq!(process_part_two(INPUT).unwrap(), "<solution>");
+    // }
+}
