b9ae04fa075a2bdfd4d14699957112aa5a723aef
Philipp Kurz <kurz_philipp@gmx.de>
2023-01-22 17:03:03 +0100

    `day-03`: Added some initial code

--- 2022/rust/day-03/src/lib.rs
+++ 2022/rust/day-03/src/lib.rs
@@ -1,9 +1,29 @@
 pub fn process_part_one(input: &str) -> Result<String, &'static str> {
+    let result = 0;
+    let mut has_item = vec![false; 52];
+    for line in input.lines() {
+        for v in &mut has_item {
+            *v = false;
+        }
+        let second_half_start = line.len() / 2;
+        for char in line.chars() {
+
+        }
+    }
     Ok(input.to_string())
 }
 
 pub fn process_part_two(input: &str) -> Result<String, &'static str> {
     Ok(input.to_string())
+}
+
+fn get_priority(ch: char) -> Result<u32, &'static str> {
+    static A_ASCII: u32 = 'a'.to_digit(10)
+        .expect("Constant letter will always be converted successfully");
+    match ch.to_digit(10) {
+        Some(val) => Ok(val - A_ASCII + 1),
+        None => Err("failed to convert character")
+    }
 }
 
 #[cfg(test)]
